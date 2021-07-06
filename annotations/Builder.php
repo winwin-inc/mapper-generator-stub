3d6b758f41c6ef46e05c1c3c58fd927b7a615fad
Ye Wenbin <yewenbin@winwin-inc.com>
2021-07-06 14:40:12 +0800

    add builder annotation

---
+++ annotations/Builder.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace winwin\mapper\annotations;
+
+use Doctrine\Common\Annotations\Annotation\Target;
+
+/**
+ * @Annotation
+ * @Target({"CLASS"})
+ */
+class Builder
+{
+}
